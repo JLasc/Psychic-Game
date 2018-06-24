603dd7defe45c48b8049afff19116acc8957113f
JLasc <lascaze.j@gmail.com>
2018-06-24 11:31:10 -0400

    psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+/* Start of Psuedo-code */
+
+/* 
+1. The Psychic Game pits a user against the computer in a guessing game. 
+2. At game start, the computer will choose a letter A-Z.
+3. The user will guess the letter by pressing keys on their keyboard.
+4. The user will only have 10 tries to guess the correct word.
+5. After 10 tries, unless the user guesses correctly, the game will reset.
+6. User score respectively to win/loss will increment 
+
+*/
